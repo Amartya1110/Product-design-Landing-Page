74a43d1cdf6d3898d4fb1a064c47774ea2646eba
Amartya Khan <amartyakhan999@gmail.com>
2022-08-19 11:51:05 +0530

    Responsive Version

---
+++ navbar.js
@@ -0,0 +1,16 @@
+const hamMenu = document.querySelector(".ham-menu");
+const navMenu = document.querySelector(".nav-list-items");
+
+hamMenu.addEventListener("click", () => {
+    navMenu.classList.toggle("toggle-nav");
+})
+
+
+function removeClass() {
+    if(window.innerWidth >= 920) {
+        navMenu.classList.remove("toggle-nav");
+    }
+}
+
+
+window.addEventListener("resize", () => removeClass())
